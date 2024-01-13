f4b0cd726ff8c08cdaba0a19f224eabeb42f2a32
mmwandera <mmwandera74@gmail.com>
2024-01-13 10:51:27 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
+#Write a function oxford_comma() in the lib/oxford_comma.py file that takes an array of string elements as an argument and converts it into a string using the Oxford comma.
 def oxford_comma(items):
-    return None
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+    if len(items) > 2:
+        return ', '.join(items)
+    else:
+        return ' '.join(items)
